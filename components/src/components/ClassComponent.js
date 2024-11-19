5dfceba35ecab1d5b3269be638e05a7572df78b1
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-11-19 18:49:25 +0500

    Class Component

---
+++ components/src/components/ClassComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ClassComponent extends React.Component {
+  render() {
+    return <h1 className='text-3xl font-bold text-center mt-6'>Class Component</h1>;
+  }
+}
+
+export default ClassComponent;

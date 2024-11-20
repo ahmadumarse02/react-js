9d6134ae2d8be9ad664e69b82cbb41263b265168
ahmadumarse02 <ahmadumarse02@gmail.com>
2024-11-20 12:25:19 +0500

    useState

---
+++ state/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Form from './components/Form'
+// import Counter from './components/Counter'
+// import BgChanger from './components/BgChanger'
+
+function App() {
+  return (
+    <>
+      {/* <BgChanger /> */}
+      {/* <Counter /> */}
+      <Form />
+    </>
+  )
+}
+
+export default App
